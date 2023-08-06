0446feaab314a3d97d869d9240c51c5dff9dfde2
sangyunpark99 <psangyun5108@gmail.com>
2023-08-06 22:57:29 +0900

    귤고르기

--- src/프로그래머스/멀리뛰기/Solution.java
+++ src/프로그래머스/멀리뛰기/Solution.java
@@ -8,9 +8,16 @@ public class Solution {
     public long solution(int n) {
         long answer = 0;
 
-        // 1 -> (1)
-        // 2 -> (1,1) (2)
-        // 3 -> (1,1,1), (1,2), (2,1),
+        long[] dp = new long[20001];
+
+        dp[0] = 1L;
+        dp[1] = 1L;
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = (dp[i-1]+dp[i-2])%1234567;
+        }
+
+        answer = dp[n];
 
         return answer;
     }
