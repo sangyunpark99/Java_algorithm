d6790a380f446f740a9e418da10277ca1922449e
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-09-29 17:22:27 +0900

    보석도둑

--- src/큰돌의터전알고리즘강의/five주차/보석도둑/Main.java
+++ src/큰돌의터전알고리즘강의/five주차/보석도둑/Main.java
@@ -5,92 +5,69 @@ import java.util.*;
  // 3프로 시간 초과
 public class Main {
 
+    public static class Jewelry{
+        int weight;
+        int price;
+
+        public Jewelry(int weight, int price){
+            this.weight = weight;
+            this.price = price;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stk = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(stk.nextToken());
-        int k = Integer.parseInt(stk.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[][] jwely = new int[n][2];
+        Jewelry[] jewelries = new Jewelry[N];
 
-        for (int i = 0; i < n; i++) { // 보석 크기, 가격 받아오기
-            stk = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int price = Integer.parseInt(st.nextToken());
 
-            int weight = Integer.parseInt(stk.nextToken());
-            int price = Integer.parseInt(stk.nextToken());
-
-            jwely[i][0] = weight;
-            jwely[i][1] = price;
+            jewelries[i] = new Jewelry(weight,price);
         }
 
-        int[] backWeights = new int[k]; // 가방 무게
-
-        for (int i = 0; i < k; i++) { // 가방 무게 받아오기
-            int maxWeight = Integer.parseInt(br.readLine());
-            backWeights[i] = maxWeight; // 가방 무게
-        }
-
-        // 1. 보석의 가격을 기준으로 정렬, 단 가격이 같은 경우 크기를 기준으로 정렬
-
-        Arrays.sort(jwely, new Comparator<int[]>() {
+        Arrays.sort(jewelries, new Comparator<Jewelry>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
+            public int compare(Jewelry o1, Jewelry o2) {
 
-                if(Objects.equals(o1[1], o2[1])){ // 가격이 같은 경우
-                    return o1[0] - o2[1];
+                if(o1.weight == o2.weight){
+                    return o2.price - o1.price;
                 }
-                return o2[1] - o1[1]; // 가격은 내림차순 정렬
+
+                return o1.weight - o2.weight;
             }
         });
 
-        int answer = 0; // 보석의 총 가격
-
-        // 가방에 담기
-        // 보석 무게와 제일 근접한 가방에 담기 가방 무게와 보석무게의 차이가 가장 작은 가방에 담기
-
-        int getJwely = 0;
-        for (int i = 0; i < jwely.length; i++) { // 보석을 하나씩 돌면서
-
-            if(getJwely == k){ // 가방 갯수 만큼 보석을 뽑은 경우 - 반복문 종
-                System.out.println(answer);
-                return;
-            }
-
-            int jwelyWeight = jwely[i][0]; // 보석 무게
-            int jwelyPrice = jwely[i][1];
-
-            // 차이가 작은 가방에 넣어주기
-            ArrayList<Integer> weightGap = new ArrayList<>(); // 보석과 가방의 무게 차이
-            for (int j = 0; j < backWeights.length; j++) {
-                if(jwelyWeight <= backWeights[j]){ // 보석보다 가방무게가 크거나 같은 경우
-                    weightGap.add(backWeights[j] - jwelyWeight); // 가방과 보석의 차이를 넣어줌
-                }
-            }
-
-            // 차이가 가장 작은 가방이 담을 수 있는 무게를 0으로 만들어 못담게 하기
-            // 보석을 담은 걸 체크
-            if(!weightGap.isEmpty()){ // 비어있지 않은 경우 -> 충분히 담을 수 있는 경우
-                int minIdx = -1;
-                int minValue = Integer.MAX_VALUE;
-                for (int j = 0; j < weightGap.size(); j++) { // 갭을 돌면서 제일 작은 사이즈 찾기
-                    int gap = weightGap.get(j);
-                    if(minValue > gap){
-                        minValue = gap;
-                        minIdx = j;
-                    }
-                }
-
-                backWeights[minIdx] = 0;
-                getJwely++; // 보석 뽑음
-                answer += jwelyPrice;// 뽑은 보석의 가격을 더해주어야 함
-            }
-
+        int[] bags = new int[K];
+        for (int i = 0; i < K; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
         }
 
-        bw.write(String.valueOf(answer));
-        bw.close();
+        Arrays.sort(bags); // 오름 차순 정렬
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        long ans = 0;
+        for (int i = 0, j = 0; i < K; i++) { // 가방을 하나씩 확인하면서
+
+            while(j < N && jewelries[j].weight <= bags[i]){ // j는 처음부터 x
+                pq.offer(jewelries[j++].price);
+            }
+
+            if(!pq.isEmpty()){ // pq가 비어있지 않은 경우
+                ans += pq.poll(); // 제일 무게가 무거운 친구
+            }
+        }
+
+        bw.write(ans + "\n");
+        bw.flush();
         br.close();
+        bw.close();
     }
 }
