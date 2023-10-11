ad6fb48bf912c6d57a562d7af822fe5b745adc5d
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-11 09:05:05 +0900

    백준 칠무해

--- src/큰돌의터전알고리즘강의/five주차/칠무해/Main.java
+++ src/큰돌의터전알고리즘강의/five주차/칠무해/Main.java
@@ -1,30 +1,37 @@
 package 큰돌의터전알고리즘강의.five주차.칠무해;
 
 import java.io.*;
-import java.util.Objects;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
+    // java는 우선순위 큐의 우선순위가 값이 작은데에 귀결된다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Float> pq = new PriorityQueue<>();
+        PriorityQueue<Float> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
         for (int i = 0; i < N; i++) {
 
-            float score = Float.parseFloat(br.readLine());
-            pq.offer(score);
+            float num = Float.parseFloat(br.readLine());
+
+            if(pq.size() == 7){ // 우선순위 교체
+                pq.add(num);
+                pq.poll(); // 제일 큰거 제거
+            }else pq.add(num);
+
+        }
+
+        PriorityQueue<Float> result = new PriorityQueue<>();
+
+        while(!pq.isEmpty()){
+            result.add(pq.poll());
         }
 
         for (int i = 0; i < 7; i++) {
-            bw.write(Objects.requireNonNull(pq.poll()).toString()+"00"+ '\n');
+            System.out.printf("%.3f\n",result.poll());
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
