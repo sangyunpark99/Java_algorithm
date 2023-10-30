51db3562459a041a5788eccd9aa39b8fa34369e0
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-10-30 19:00:35 +0900

    미세먼지 안녕

--- src/큰돌의터전알고리즘강의/five주차/미세먼지안녕/Main.java
+++ src/큰돌의터전알고리즘강의/five주차/미세먼지안녕/Main.java
@@ -3,18 +3,26 @@ package 큰돌의터전알고리즘강의.five주차.미세먼지안녕;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int[][] map;
-    private static List<Integer> cleanerY;
+    private static int R, C, T, map[][];
+    private static int cleaner = -1;
+    private static Queue<Dust> dusts;
+    private static int[] dx = {0, 1, 0, -1}, dy = {-1, 0, 1, 0};
 
-    private static int R;
-    private static int C;
-    private static int T;
+    private static class Dust {
+        int x, y, w;
+
+        public Dust(int y, int x, int w) {
+            this.y = y;
+            this.x = x;
+            this.w = w;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,113 +33,125 @@ public class Main {
         T = Integer.parseInt(st.nextToken());
 
         map = new int[R][C];
+        dusts = new LinkedList<>();
 
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
-            int j = 0;
-            while (st.hasMoreTokens()) {
+            for (int j = 0; j < C; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                j++;
-            }
-        }
 
-        cleanerY = new ArrayList<>();
-
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                if (map[i][j] == -1) {
-                    cleanerY.add(i);
-                }
-
-                if (cleanerY.size() == 2) { // 더이상 탐지할 필요가 없다.
-                    break;
+                if (cleaner == -1 && map[i][j] == -1) {
+                    cleaner = i;
                 }
             }
         }
 
-        // 1. 먼지 탐방
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                spreadDust(i, j);
-            }
+        for (int i = 0; i < T; i++) {
+            checkDust();
+            spread();
+            windTop();
+            windBottom();
         }
+
+        int res = 0;
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
                 System.out.print(map[i][j] + " ");
+                if (map[i][j] == -1) {
+                    continue;
+                }
+                res += map[i][j];
             }
             System.out.println();
         }
 
-        // 2. 청소기로 먼지 돌리기
-        //firstWindMove();
-        //secondWindMove();
+        System.out.println(res);
     }
 
-    private static void spreadDust(int curY, int curX) { // 먼지 움직이게 하기
+    private static void checkDust() {
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (map[i][j] == -1 || map[i][j] == 0) {
+                    continue;
+                }
+                dusts.add(new Dust(i, j, map[i][j]));
+            }
+        }
+    }
 
-        int[] dy = new int[]{-1, 0, 1, 0};
-        int[] dx = new int[]{0, 1, 0, -1};
-
-        int dustDegree = map[curY][curX];
-
-        int spreadDirection = 0;
-
-        for (int i = 0; i < 4; i++) { // 4방향 확산
-            int ny = curY + dy[i];
-            int nx = curX + dx[i];
-
-            if (ny < 0 || ny >= R || nx < 0 || nx >= C || map[ny][nx] == -1) { // 범위 만족
+    private static void spread() {
+        while (!dusts.isEmpty()) {
+            Dust cur = dusts.poll();
+            if (cur.w < 5) {
                 continue;
             }
+            int amountSpread = cur.w / 5;
+            int cnt = 0;
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-            spreadDirection++;
-            map[ny][nx] += (int) (dustDegree / 5);
+                if (nx < 0 || nx >= C || ny < 0 || ny >= R) {
+                    continue;
+                }
+                if (map[ny][nx] == -1) {
+                    continue;
+                }
+
+                map[ny][nx] += amountSpread;
+                cnt++;
+            }
+
+            map[cur.y][cur.x] -= amountSpread * cnt;
         }
-
-        map[curY][curX] = dustDegree - dustDegree / 5 * spreadDirection;
     }
 
-    private static void firstWindMove() {
-        // 외곽만 땡기면된다.
-        for (int i = cleanerY.get(0); i > 0; i--) { // 위 -> 아래
+    private static void windTop() {
+        int top = cleaner;
+        // 아래로 당기기
+        for (int i = top - 1; i > 0; i--) {
             map[i][0] = map[i - 1][0];
         }
 
-        for (int i = 1; i < map[0].length - 1; i++) { // 오 -> 왼
+        // 왼쪽으로 당기기
+        for (int i = 0; i < C - 1; i++) {
             map[0][i] = map[0][i + 1];
         }
 
-        for (int i = 1; i < cleanerY.get(0); i++) {  // 아래 -> 위
-            map[i][0] = map[i + 1][0];
+        // 위로 당기기
+        for (int i = 0; i < top; i++) {
+            map[i][C - 1] = map[i + 1][C - 1];
         }
 
-        for (int i = map[0].length - 1; i > 0; i++) {
-            map[0][i] = map[0][i - 1];
+        // 오른쪽으로 당기기
+        for (int i = C - 1; i > 1; i--) {
+            map[top][i] = map[top][i - 1];
         }
-        //청소기 위치 계속 -1로 초기화
-        map[0][cleanerY.get(1)] = -1;
+
+        map[top][1] = 0; // 깨끗한 공간
     }
 
-    private static void secondWindMove() {
-        // 외곽만 땡기면된다.
-        for (int i = cleanerY.get(1); i < map.length - 1; i++) {  // 아래 -> 위
+    private static void windBottom() {
+        int bottom = cleaner + 1;
+
+        // 위로 당기기
+        for (int i = bottom + 1; i < R - 1; i++) {
             map[i][0] = map[i + 1][0];
         }
-
-        for (int i = 0; i < C - 1; i++) { // 오 -> 왼
+        // 왼쪽으로 당기기
+        for (int i = 0; i < C - 1; i++) {
             map[R - 1][i] = map[R - 1][i + 1];
         }
-
-        for (int i = R - 1; i >= cleanerY.get(1); i--) { // 위 -> 아래
-            map[i][0] = map[i - 1][0];
+        // 아래로 당기기
+        for (int i = R - 1; i > bottom; i--) {
+            map[i][C - 1] = map[i - 1][C - 1];
+        }
+        // 오른쪽으로 당기기
+        for (int i = C - 1; i > 1; i--) {
+            map[bottom][i] = map[bottom][i - 1];
         }
 
-        for (int i = C - 1; i > 0; i++) { // 왼 -> 오
-            map[cleanerY.get(1)][i] = map[cleanerY.get(i)][i - 1];
-        }
-
-        //청소기 위치 계속 -1로 초기화
-        map[0][cleanerY.get(1)] = -1;
+        map[bottom][1] = 0; // 깨끗한 공간
     }
 }
