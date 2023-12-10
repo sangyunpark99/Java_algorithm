d1d418a0be0cb789a8ef738c38351fda44959bd7
psangyun5108@gmail.com <psangyun5108@gmail.com>
2023-12-10 23:00:41 +0900

    용돈관리

--- src/큰돌의터전알고리즘강의/three주차/용돈관리/Main.java
+++ src/큰돌의터전알고리즘강의/three주차/용돈관리/Main.java
@@ -1,7 +1,25 @@
 package 큰돌의터전알고리즘강의.three주차.용돈관리;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+
+    public static int N;
+    public static int M;
+    public static int[] money;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            money[i] = Integer.parseInt(br.readLine());
+        }
     }
 }
