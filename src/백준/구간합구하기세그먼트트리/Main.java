c15986ed303bd8e11b836ca87ccef61a63576d6d
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-12 17:01:47 +0900

    구간합-세그먼트리

--- src/백준/구간합구하기세그먼트트리/Main.java
+++ src/백준/구간합구하기세그먼트트리/Main.java
@@ -8,9 +8,10 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    static long[] arr, tree;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static long[] arr, tree;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,29 +20,27 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        arr = new long[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Long.parseLong(br.readLine());
-        }
-
+        arr = new long[N + 1]; // index 1부터 시작
         tree = new long[N * 4];
 
-        init(1, N, 1);
+        for (int i = 1; i <= N; i++) { // 배열 초기화
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
+        init(1, N, 1); // 세그먼트 트리 초기화
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            if (a == 1) {
-                long dif = c - arr[b];
-                arr[b] = c;
-                update(1, N, 1, b, dif);
-            } else if (a == 2) {
-                sb.append(sum(1, N, 1, b, (int) c) + "\n");
+            if (a == 1) { // 값 업데이트
+                long diff = c - arr[b]; // 이 차이만큼 다른 구간에 더해주어야 한다.
+                //arr[b] = c; 굳이 필요하지 않다.
+                update(1, N, 1, b, diff);
+            } else { // 구간합 구하기
+                sb.append(sum(1, N, 1, b, c) + "\n");
             }
         }
 
@@ -51,54 +50,41 @@ public class Main {
         br.close();
     }
 
-    // start: 시작 인덱스, end: 끝 인덱스
-    public static long init(int start, int end, int node) {
-        if (start == end) {
-            return tree[node] = arr[start];
-        }
-
-        int mid = (start + end) / 2;
-
-        // 재귀적으로 두 부분으로 나눈 뒤에 그 합을 자기 자신으로 함.
-        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
-    }
-
-    // start: 시작 인덱스, end: 끝 인덱스
-    // left, right: 구간 합을 구하고자 하는 범위
     public static long sum(int start, int end, int node, int left, int right) {
-        // 범위 밖에 있는 경우
         if (left > end || right < start) {
             return 0;
         }
 
-        // 범위 안에 있는 경우
-        if (left <= start && end <= right) {
+        if (left <= start && end <= right) { // 범위 내에 있는 경우
             return tree[node];
         }
 
-        // 그렇지 않다면, 두 부분으로 나누어 합을 구하기
         int mid = (start + end) / 2;
         return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
     }
 
-    // start: 시작 인덱스, end: 끝 인덱스
-    // idx: 구간 합을 수정하고자 하는 노드
-    // dif: 수정할 값
-    public static void update(int start, int end, int node, int idx, long dif) {
-        // 범위 밖에 있는 경우
+    public static long init(int start, int end, int node) {
+        if (start == end) { // 리프노드
+            return tree[node] = arr[start];
+        }
+
+        int mid = (start + end) / 2;
+        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+    }
+
+    public static void update(int start, int end, int node, int idx, long diff) {
         if (idx < start || idx > end) {
             return;
         }
 
-        // 범위 안에 있으면 내려가며 다른 원소도 갱신
-        tree[node] += dif;
-        if (start == end) {
+        tree[node] += diff; // 차이만큼 더해주어야 한다.(나머지 모든 구간합에)
+
+        if (start == end) { // 리프 노드에 도달한 경우
             return;
         }
 
         int mid = (start + end) / 2;
-        update(start, mid, node * 2, idx, dif);
-        update(mid + 1, end, node * 2 + 1, idx, dif);
+        update(start, mid, node * 2, idx, diff);
+        update(mid + 1, end, node * 2 + 1, idx, diff);
     }
-
 }
