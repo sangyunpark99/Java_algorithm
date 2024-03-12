2ee3bc123ed64649085cbf1b1e73ad8b3fe160a8
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-12 14:43:08 +0900

    펜윅트리

--- src/백준/일이더하기사/Main.java
+++ src/백준/일이더하기사/Main.java
@@ -5,32 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-        int[][] dp = new int[10001][4];
-        int t = Integer.parseInt(br.readLine());
+        // 1,2,3으로 더해지는 수
+        int[][] dp = new int[10004][4];
 
-        // 합이 3이하인 경우 사전에 초기값 1을 넣어준다.
+        // 아래에 존재하는 수들은 전부 1개씩 존재한다.
         dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
 
         for (int i = 4; i <= 10000; i++) {
-            dp[i][1] = dp[i - 1][1];
-            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][1] = dp[i - 1][1]; // 각 경우에 1을 더하는 경우
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2]; // 각 경우에 2를 더하는 경우
             dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+            // 3은 왜 3개인가? 3을 나타내는 경우는 3가지이다. 1+1+1, 2+1, 3
         }
 
-        for (int i = 0; i < t; i++) {
+        for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(dp[n][1] + dp[n][2] + dp[n][3]);
-            if (i < t - 1) {
+            int value = dp[n][1] + dp[n][2] + dp[n][3];
+
+            sb.append(value);
+            if (i < T - 1) {
                 sb.append("\n");
             }
         }
 
         System.out.println(sb);
-
         br.close();
     }
 }
--- src/백준/자두나무/Main.java
+++ src/백준/자두나무/Main.java
@@ -17,36 +17,40 @@ public class Main {
         T = Integer.parseInt(st.nextToken());
         W = Integer.parseInt(st.nextToken());
 
-        zadu = new int[T + 1];
+        zadu = new int[T + 1]; // 1초부터 시작
 
         for (int i = 1; i <= T; i++) {
             zadu[i] = Integer.parseInt(br.readLine());
         }
 
-        int[][][] dp = new int[3][T + 1][W + 2];
+        // 나무 위치 | 시간 | 움직임
+        int[][][] dp = new int[3][T + 1][W + 2]; // 움직임이 없는 경우가 0 이되면 -1을 할경우 오류가 발생한다.
 
-        for (int i = 1; i <= T; i++) { // 시간
-            for (int j = 1; j <= W + 1; j++) { // 이동 횟수
-                if (zadu[i] == 1) {
-                    // 가만히 있다가 먹거나 아니면 이동해서 먹거나 둘 중 큰걸로 먹어~
+        for (int i = 1; i <= T; i++) { // 시간에 따른
+            for (int j = 1; j <= W + 1; j++) { // 움직임
+                if (zadu[i] == 1) { // 1번 나무 위치에 자두가 존재한다면
+                    // 1번나무에서 가만히 있음으로 자두를 먹가나 2번나무에서 1번나무로 이동해서 자두를 먹은 갯수중 최댓값으로 갱신
                     dp[1][i][j] = Math.max(dp[1][i - 1][j] + 1, dp[2][i - 1][j - 1] + 1);
+                    // 2번 나무에서 가만히 있는 경우와 1번 나무에서 2번나무로 이동한 경우 중 최댓값으로 갱신
                     dp[2][i][j] = Math.max(dp[2][i - 1][j], dp[1][i - 1][j - 1]);
-                } else {
-                    if (i == 1 && j == 1) { // 어차피 못먹어~
+                } else { // 2번나무에 자두가 존재한다면
+                    if (i == 1 && j == 1) { // 맨처음 움직임 횟수가 0번인 경우, 애초에 2번 자두나무로 못 옮기는 경우
                         continue;
                     }
+                    // 1번나무에서 가만히 있는 경우와 2번 나무에서 1번나무로 이동한 경우 중 최댓값으로 갱신
                     dp[1][i][j] = Math.max(dp[1][i - 1][j], dp[2][i - 1][j - 1]);
-                    dp[2][i][j] = Math.max(dp[1][i - 1][j - 1] + 1, dp[2][i - 1][j] + 1);
+                    // 2번나무에서 가만히 자두를 먹는 경우와 1번 나무에서 2번 나무로 옮겨서 자두를 먹은 갯수의 최댓값으로 갱신
+                    dp[2][i][j] = Math.max(dp[2][i - 1][j] + 1, dp[1][i - 1][j - 1] + 1);
                 }
             }
         }
 
         int result = Integer.MIN_VALUE;
-
         for (int i = 1; i <= W + 1; i++) {
             result = Math.max(result, Math.max(dp[1][T][i], dp[2][T][i]));
         }
 
         System.out.println(result);
+        br.close();
     }
 }
