f9619332234fe8ee7c8854f6795ca9bcc5ca1261
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-13 13:07:16 +0900

    세그먼트 트리

--- src/백준/구간합구하기세그먼트트리/Main.java
+++ src/백준/구간합구하기세그먼트트리/Main.java
@@ -9,7 +9,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static long[] arr, tree;
+    public static long[] arr;
+    public static long[] tree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,71 +21,77 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        arr = new long[N + 1]; // index 1부터 시작
-        tree = new long[N * 4];
+        arr = new long[N + 1];
+        tree = new long[4 * N];
 
-        for (int i = 1; i <= N; i++) { // 배열 초기화
-            arr[i] = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
         }
 
-        init(1, N, 1); // 세그먼트 트리 초기화
+        // 트리의 노드는 1번부터 시작
+        init(1, N, 1);
+
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
 
-            if (a == 1) { // 값 업데이트
-                long diff = c - arr[b]; // 이 차이만큼 다른 구간에 더해주어야 한다.
-                //arr[b] = c; 굳이 필요하지 않다.
-                update(1, N, 1, b, diff);
-            } else { // 구간합 구하기
-                sb.append(sum(1, N, 1, b, c) + "\n");
+            if (a == 1) { // 수 변경
+                long dif = c - arr[b]; // 변경된 수가 또 변경될 수 있다.
+                arr[b] = c;
+                update(1, N, 1, b, dif);
+            } else if (a == 2) { // 구간의 합 구하기
+                sb.append(sum(1, N, 1, b, (int) c) + "\n");
             }
         }
 
         bw.write(sb.toString());
         bw.flush();
-        bw.close();
         br.close();
-    }
-
-    public static long sum(int start, int end, int node, int left, int right) {
-        if (left > end || right < start) {
-            return 0;
-        }
-
-        if (left <= start && end <= right) { // 범위 내에 있는 경우
-            return tree[node];
-        }
-
-        int mid = (start + end) / 2;
-        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+        bw.close();
     }
 
     public static long init(int start, int end, int node) {
-        if (start == end) { // 리프노드
+        if (start == end) { // 리프 노드
             return tree[node] = arr[start];
         }
 
+        // 분할해서
         int mid = (start + end) / 2;
         return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
     }
 
-    public static void update(int start, int end, int node, int idx, long diff) {
-        if (idx < start || idx > end) {
+    // 재귀 함수는 종료조건이 분명해야 한다.
+    public static void update(int start, int end, int node, int idx, long dif) {
+        if (idx < start || idx > end) { // 인덱스가 범위를 벗어난 경우
+            return;
+        }
+        tree[node] += dif;
+
+        if (start == end) { // 리프 노드인 경우
             return;
         }
 
-        tree[node] += diff; // 차이만큼 더해주어야 한다.(나머지 모든 구간합에)
-
-        if (start == end) { // 리프 노드에 도달한 경우
-            return;
-        }
-
+        // 다른 구간도 업데이트
         int mid = (start + end) / 2;
-        update(start, mid, node * 2, idx, diff);
-        update(mid + 1, end, node * 2 + 1, idx, diff);
+        update(start, mid, node * 2, idx, dif);
+        update(mid + 1, end, node * 2 + 1, idx, dif);
+    }
+
+    public static long sum(int start, int end, int node, int left, int right) {
+        if (left > end || right < start) { // 구간을 벗어난 경우
+            return 0;
+        }
+
+        if (left <= start && end <= right) { // 구간이 겹치거나 내부에 존재하는 경우
+            return tree[node];
+        }
+
+        // 겹치지 않는 부분이 생기는 경우 => 겹치지 않은 부분에 대한 합을 구해준다.
+        int mid = (start + end) / 2;
+        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
     }
 }
