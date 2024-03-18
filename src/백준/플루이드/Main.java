1212db8d75aa41263f40da84def0d991a1fc485a
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-19 08:45:18 +0900

    벨만포드

---
+++ src/백준/플루이드/Main.java
@@ -0,0 +1,73 @@
+package 백준.플루이드;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final int INF = 1_000_000_000;
+    public static int N;
+    public static int M;
+
+    public static int[][] graph;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        graph = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) { // 초기화
+            for (int j = 1; j <= N; j++) {
+                if (i == j) {
+                    graph[i][j] = 0;
+                } else {
+                    graph[i][j] = INF;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a][b] = Math.min(graph[a][b], c); // 최소거리로 해주어야 한다.
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (graph[i][j] == INF) { // INF인 경우 0
+                    graph[i][j] = 0;
+                }
+                sb.append(graph[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
