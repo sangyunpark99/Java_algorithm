d6a22bb15f90f7840f4aeca307ebf19a65c136df
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-20 10:47:18 +0900

    벨만-포드 알고리즘 복습

---
+++ src/백준/가장긴증가하는부분순열/Main.java
@@ -0,0 +1,42 @@
+package 백준.가장긴증가하는부분순열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+    public static int[] arr;
+    public static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            //dp[i] = 1;
+        }
+
+        int max = 1;
+        dp[1] = 1;
+
+        // 시간복잡도 O(N^2)
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
