f377b066fd295ddc882a54d44284a4a3fc11d7ba
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-12-12 15:25:53 +0900

    프로그래머스 lv1 - 카드 뭉치

---
+++ src/프로그래머스/lv1/Solution.java
@@ -0,0 +1,36 @@
+package 프로그래머스.lv1;
+
+public class Solution {
+
+    public static String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+
+        int cards1Idx = 0;
+        int cards2Idx = 0;
+
+        for (String word : goal) {
+            if (cards1Idx < cards1.length && cards1[cards1Idx].equals(word)) {
+                cards1Idx++;
+                continue;
+            } else if (cards2Idx < cards2.length && cards2[cards2Idx].equals(word)) {
+                cards2Idx++;
+                continue;
+            } else {
+                answer = "No";
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{
+                "i", "water", "drink"
+        }, new String[]{
+                "want", "to"
+        }, new String[]{
+                "i", "want", "to", "drink", "water"
+        }));
+    }
+}
